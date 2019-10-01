eedd3eca95e302cd30eeb40a55237f41557298eb
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-01 18:56:25 +0300

    create massive tasks

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ import Tasks from "./Tasks";
 
 
 class App extends React.Component {
+    tasks = [
+        {title:'HTML',  isDone:true, priority:'low'},
+        {title:'JS',    isDone:true, priority:'middle'},
+        {title:'React', isDone:true, priority:'high'},
+        {title:'Git',   isDone:true, priority:'high'}
+    ];
     render = () => {
         return (
             <div className="App">
                 <div className="todoList">
                     <Header />
-                    <Tasks />
+                    <Tasks tasks={this.tasks} />
                     <Footer />
 
                 </div>
