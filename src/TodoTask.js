42114b5c5c8e8cce3a01372a1e62e005e32479da
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-15 20:54:50 +0300

    currentTarget checked in input - just check task

--- src/TodoTask.js
+++ src/TodoTask.js
@@ -3,12 +3,17 @@ import './App.css';
 
 
 class TodoTask extends React.Component {
+
+    onIsDoneChanged = (e)=> {
+        alert(e.currentTarget.checked)
+    };
+
     render = () => {
         return (
 
             <div className="todoList-tasks">
                 <div className="todoList-task">
-                    <input type="checkbox" checked={this.props.isDone}/>
+                    <input type="checkbox" checked={this.props.isDone} onChange={this.onIsDoneChanged}/>
                     <span>{this.props.title}, priority- {this.props.priority}</span>
                 </div>
             </div>
