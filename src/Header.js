29941f7a4cd388d228bc091239586d3e6fc48aa6
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-23 21:27:07 +0300

    add onErrorChanged

--- src/Header.js
+++ src/Header.js
@@ -29,13 +29,18 @@ class Header extends React.Component {
         }
     };
 
+    onErrorChanged = () => {
+        this.setState({error: false})
+    };
+
+
     render = () => {
         let errorFilter = this.state.error ? 'error': '';
         return (
             <div className='todoList-header'>
                 <h3 className='todoList-header_title'>What to learn</h3>
                 <div className='todoList-newTaskForm'>
-                    <input ref={this.newTaskTitleRef} type='text' placeholder='New task name' className={errorFilter}/>
+                    <input ref={this.newTaskTitleRef} type='text' placeholder='New task name' className={errorFilter} onChange={this.onErrorChanged}/>
                     <button onClick={this.onAddTaskClick}>Add</button>
 
                 </div>
