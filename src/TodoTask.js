497ecb2238cf38932f88b20c3e5ae5d123575d90
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-30 19:03:25 +0300

    deactivate mode for edit

--- src/TodoTask.js
+++ src/TodoTask.js
@@ -18,6 +18,10 @@ class TodoTask extends React.Component {
         this.props.changeStatus( this.props.task.id, e.currentTarget.checked)
     };
 
+    deactivateEditMode = () => {
+        this.setState({editMode: false})
+    };
+
     render = () => {
         let classForDone = this.props.task.isDone === true ? 'todoList-task done': 'todoList-task';
         return (
@@ -26,7 +30,7 @@ class TodoTask extends React.Component {
                 <div className={classForDone}>
                     <input type="checkbox" checked={this.props.task.isDone} onChange={this.onIsDoneChanged}/>
                     {this.state.editMode
-                    ? <input value={this.props.task.title}/>
+                    ? <input onBlur={this.deactivateEditMode } autoFocus={true} value={this.props.task.title}/>
                     : <span onClick={this.activateEditMode}>{this.props.task.id} - title: {this.props.task.title}</span>}, priority- {this.props.task.priority}
                 </div>
             </div>
