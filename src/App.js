b8d51cfa32607bfbd658397aa2a43c2b892afc9b
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-11-06 19:05:34 +0300

    create method addTodoList for new component and add from props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import TodoList from "./TodoList";
+import AddNewItemForm from "./AddNewItemForm";
 
 
 
@@ -14,12 +15,22 @@ class App extends React.Component {
         ]
     };
 
+    addTodoList = (title) => {
+        alert(title)
+    };
+
     render = () => {
         const todoLists = this.state.todoLists.map(tl => <TodoList id={tl.id} title={tl.title}/>);
         return (
+            <>
+                <div>
+                    <AddNewItemForm addItem={this.addTodoList}/>
+                </div>
+
             <div className="App">
                 {todoLists}
             </div>
+                </>
         );
     }
 }
