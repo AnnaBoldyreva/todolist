bd850b7a615f84c71ce41489246bceee6ca80473
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-12-01 12:25:40 +0300

    add state data

--- src/store.js
+++ src/store.js
@@ -2,7 +2,11 @@ import {createStore} from "redux";
 
 
 const initialState = {
-    todolists: []
+    todolists: [
+        {'id': 0, 'title': 'today'},
+        {'id': 1, 'title': 'tomorrow'},
+        {'id': 2, 'title': 'to eat'}
+    ]
 };
 
 const reducer = (state = initialState, action) => {
